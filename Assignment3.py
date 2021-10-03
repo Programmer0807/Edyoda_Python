0ded725b41b21e0d83d72906ed7e093dba42322a
Kunal Kailashsing Rajure <78902991+Programmer0807@users.noreply.github.com>
2021-10-03 14:51:59 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,12 @@
+import math
+
+s = (1,2,3,4,5,6,7,8,9)
+b = 0
+a = 0
+for i in s:
+    if(i % 2) == 0:
+        b=b+1
+    else:
+        a=a+1
+print('Number of even numbers :',a)
+print('Number of odd numbers :',b)
