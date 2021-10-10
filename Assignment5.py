67524c8e6a906a2fb67eea78b54ca9cefedde928
Kunal Kailashsing Rajure <78902991+Programmer0807@users.noreply.github.com>
2021-10-10 20:53:28 +0530

    Delete Assignment5.py

--- Assignment5.py
+++
@@ -1,8 +0,0 @@
-
-alp = {'a':ord('a'),'b':ord('b'),'c':ord('c'),'d':ord('d'),'e':ord('e'),'f':ord('f'),'g':ord('g'),
-'h':ord('h'),'i':ord('i'),'j':ord('j'),'k':ord('k'),'l':ord('l'),'m':ord('m'),'n':ord('n')
-,'o':ord('o'),'p':ord('p'),'q':ord('q'),'r':ord('r'),'s':ord('s'),'t':ord('t'),'u':ord('u'),'v':ord('v'),
-'w':ord('w'),'x':ord('x'),'y':ord('y'),'z':ord('z')}
-
-
-print(alp)
